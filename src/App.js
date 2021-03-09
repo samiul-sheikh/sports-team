1b50fd40ce88e136bb0c4a4fc61101662972cfce
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-10 05:57:57 +0600

    setup basic route and folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from "react";
 import './App.css';
+import Home from "./Components/Home/Home";
+import NoMatch from "./Components/NoMatch/NoMatch";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                <Home/>
+            </Route>
+            <Route path="/home">
+                <Home/>
+            </Route>
+            <Route path="*">
+                <NoMatch/>
+            </Route>
+        </Switch>
+    </Router>
   );
 }
 
