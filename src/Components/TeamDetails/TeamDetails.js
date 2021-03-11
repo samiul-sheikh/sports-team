38f593d1ab22594fc05878187ec77f6588a6611f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-11 12:27:02 +0600

    display required data

--- src/Components/TeamDetails/TeamDetails.js
+++ src/Components/TeamDetails/TeamDetails.js
@@ -8,17 +8,26 @@ const TeamDetails = () => {
 
     const [team, setTeam] = useState({});
 
+    const { strTeamBanner, strTeam, intFormedYear, strCountry, strSport, strGender, strDescriptionEN } = team;
+
     useEffect(() => {
         const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => setTeam(data.teams))
+            .then(data => setTeam(data.teams[0]))
     }, [])
 
     return (
         <div>
             <h3>Team Details</h3>
-            <p>{team[0].strSport}</p>
+            <img src={strTeamBanner} alt="" />
+            <p>{strTeam}</p>
+            <p>Founded:{intFormedYear}</p>
+            <p>Country:{strCountry}</p>
+            <p>Sport Type:{strSport}</p>
+            <p>Gender:{strGender}</p>
+            <p>Description:{strDescriptionEN}</p>
+
         </div>
     );
 };
